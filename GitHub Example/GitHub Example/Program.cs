0040a86794bb2315be31e5c11bde2d80270a6220
User-Bex <110834358+User-Bex@users.noreply.github.com>
2023-09-04 10:19:35 +0200

    Should be good to use now

--- GitHub Example/GitHub Example/Program.cs
+++ GitHub Example/GitHub Example/Program.cs
@@ -5,7 +5,7 @@ public class Program{
     static void Main(){
         HogwartsStudent[] hogwartsStudents = {
             //new HogwartsStudentHenk(100, 20, House.Slytherin),
-        }
+        };
 
         foreach(HogwartsStudent s in hogwartsStudents){
             WriteLine(s);
