6a30e07caa98dfaf8112ef8c18d755c232e273ae
Henk <henk.scheepers.hd@gmail.com>
2023-09-04 09:52:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using static System.Console;
+
+public class Program{
+
+    static void Main(){
+        HogwartsStudent[] hogwartsStudents = {
+            //new HogwartsStudentHenk(100, 20, House.Slytherin),
+        }
+
+        foreach(HogwartsStudent s in hogwartsStudents){
+            WriteLine(s);
+        }
+    }
+
+}
