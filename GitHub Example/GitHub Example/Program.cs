18d5d5799cf5ac87ac7bc5a78f16636cca165027
cait15 <caitlinjacobs0815@gmail.com>
2023-09-04 11:09:08 +0200

    CAITS CLASS

--- GitHub Example/GitHub Example/Program.cs
+++ GitHub Example/GitHub Example/Program.cs
@@ -1,10 +1,11 @@
+using GitHub_Example;
 using static System.Console;
 
 public class Program{
 
     static void Main(){
         HogwartsStudent[] hogwartsStudents = {
-            //new HogwartsStudentHenk(100, 20, House.Slytherin),
+            new HogwartStudentCait(),
         };
 
         foreach(HogwartsStudent s in hogwartsStudents){
