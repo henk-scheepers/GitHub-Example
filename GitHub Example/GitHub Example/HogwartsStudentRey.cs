e66aa02aeceed60a67ea5f07c4b4c878eac1fe17
ReynardOtto <reynardotto03@gmail.com>
2023-09-04 20:37:04 +0200

    Added HogwartStudentRey class

---
+++ GitHub Example/GitHub Example/HogwartsStudentRey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Example
+{
+    public class HogwartsStudentRey : HogwartsStudent
+    {
+        public HogwartsStudentRey() : base(90, 270, House.Gryffindor)
+        {
+        }
+        public override string Name 
+        {
+            get { return "Reynard"; }
+        }
+
+        protected override string SayCatchPhrase()
+        {
+            string catchPhrase = "It do be like that sometimes";
+            return catchPhrase;
+        }
+        protected override string CastSpell()
+        {
+            string spell = "AtomicBombious";
+            return spell;
+        }
+    }
+}
